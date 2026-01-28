e0d4dc598ad0f6ca514b59e8679a7d2d980f4896
xiaoming <1038761793@qq.com>
2026-01-28 12:14:23 +0800

    feat: 移除 roboto 字体

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,5 @@
 import "./app.css";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
